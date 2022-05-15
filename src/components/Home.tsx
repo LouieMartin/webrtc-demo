95fdd481e99a18b55fd5452809f7f30f7612c2cd
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2022-05-15 17:15:43 +0800

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,98 +1,113 @@
-import { TextInput, Button, Center, Loader, Title, Text, Box } from '@mantine/core';
 import { MediaConnection, Peer } from 'peerjs';
+import { useForm } from '@mantine/form';
 import * as React from 'react';
+import {
+  TextInput,
+  Center,
+  Button,
+  Loader,
+  Text,
+  Box,
+} from '@mantine/core';
 
 type Props = {}
 
 export const Home: React.FC<Props> = (_: Props) => {
-    const userVideo = React.useRef<HTMLVideoElement | null>(null);
-    const [inCall, setInCall] = React.useState<boolean>(false);
-    const video = React.useRef<HTMLVideoElement | null>(null);
-    const [userId, setUserId] = React.useState<string>('');
-    const [peer, setPeer] = React.useState<Peer>();
+  const userVideo = React.useRef<HTMLVideoElement | null>(null);
+  const [inCall, setInCall] = React.useState<boolean>(false);
+  const video = React.useRef<HTMLVideoElement | null>(null);
+  const [peer, setPeer] = React.useState<Peer>();
+  const callForm = useForm({
+    initialValues: {
+      userId: '',
+    },
+  });
 
-    React.useEffect(() => {
-        const newPeer = new Peer();
+  React.useEffect(() => {
+    const newPeer = new Peer();
 
-        newPeer.on('open', () => setPeer(newPeer));
-        newPeer.on('call', (call: MediaConnection) => {
-            setInCall(oldState => !oldState);
-            navigator.mediaDevices.getUserMedia({
-                audio: true,
-                video: true,
-            }).then(stream => {
-                if (video.current) {
-                    video.current.srcObject = stream;
-                }
+    newPeer.on('open', () => setPeer(newPeer));
+    newPeer.on('call', (call: MediaConnection) => {
+      setInCall(oldState => !oldState);
+      navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      }).then(stream => {
+        if (video.current) {
+          video.current.srcObject = stream;
+        }
 
-                call.answer(stream);
-                call.on('stream', (userStream: MediaStream) => {
-                    if (userVideo.current) {
-                        userVideo.current.srcObject = userStream;
-                    }
-                });
-
-                call.on('close', () => {
-                    setInCall(oldState => !oldState);
-                });
-            });
+        call.answer(stream);
+        call.on('stream', (userStream: MediaStream) => {
+          if (userVideo.current) {
+            userVideo.current.srcObject = userStream;
+          }
         });
 
-        return () => newPeer.destroy();
-    }, []);
+        call.on('close', () => {
+          setInCall(oldState => !oldState);
+        });
+      });
+    });
 
-    return (
-        <Center sx={{ height: '100vh' }}>
-            {peer ? (
-                <Box p="md">
-                    {!inCall ? (
-                        <>
-                            <Title align="center">WebRTC Demo</Title>
-                            <Text color="dimmed" align="center" mt="xl">
-                                A WebRTC demo or example built with Mantine, React.js, Vite.js, and PeerJS.
-                                User ID: {peer?.id}
-                            </Text>
-                            <TextInput onChange={event => setUserId(event.target.value)} placeholder="User ID here" mt="md" />
-                            <Button
-                                onClick={() => {
-                                    setInCall(oldState => !oldState);
-                                    navigator.mediaDevices.getUserMedia({
-                                        audio: true,
-                                        video: true,
-                                    }).then(stream => {
-                                        if (video.current) {
-                                            video.current.srcObject = stream;
-                                        }
+    return () => newPeer.destroy();
+  }, []);
 
-                                        const call = peer.call(userId, stream);
+  return (
+    <Center sx={{ height: '100vh' }}>
+      {peer ? (
+        <Box p="md">
+          {inCall ? (
+            <>
+              <video style={{ borderRadius: 16, marginRight: 16 }} ref={video} autoPlay muted />
+              <video style={{ borderRadius: 16, marginRight: 16 }} ref={userVideo} autoPlay />
+            </>
+          ) : (
+            <>
+              <Text color="dimmed" align="center">{peer.id}</Text>
+              <form onSubmit={callForm.onSubmit(values => {
+                setInCall(oldState => !oldState);
+                navigator.mediaDevices.getUserMedia({
+                  audio: true,
+                  video: true,
+                }).then(stream => {
+                  if (video.current) {
+                    video.current.srcObject = stream;
+                  }
 
-                                        call.on('stream', (userStream: MediaStream) => {
-                                            if (userVideo.current) {
-                                                userVideo.current.srcObject = userStream;
-                                            }
-                                        });
+                  const call = peer.call(values.userId, stream);
 
-                                        call.on('close', () => {
-                                            setInCall(oldState => !oldState);
-                                        });
-                                    });
-                                }}
-                                fullWidth
-                                mt="md"
-                            >
-                                Call
-                            </Button>
-                        </>
-                    ) : (
-                        <>
-                            <video autoPlay ref={video} muted />
-                            <video autoPlay ref={userVideo} />
-                        </>
-                    )}
-                </Box>
-            ) : (
-                <Loader variant="dots" />
-            )}
-        </Center>
-    );
+                  call.on('stream', (userStream: MediaStream) => {
+                    if (userVideo.current) {
+                      userVideo.current.srcObject = userStream;
+                    }
+                  });
+
+                  call.on('close', () => {
+                    setInCall(oldState => !oldState);
+                  });
+                });
+              })}>
+                <TextInput
+                  {...callForm.getInputProps('userId')}
+                  placeholder="User ID here"
+                  mt="md"
+                />
+                <Button
+                  type="submit"
+                  radius="xl"
+                  fullWidth
+                  mt="md"
+                >
+                  Call
+                </Button>
+              </form>
+            </>
+          )}
+        </Box>
+      ) : (
+          <Loader variant="dots" />
+      )}
+    </Center>
+  );
 };
